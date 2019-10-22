b88e4b93381cf31b730f1c5f4bab77250c5548be
ivano2000 <ivano.salima@gmail.com>
2019-10-22 18:20:54 +0200

    start json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import "./style.css";
-import { devineLog } from "./js/utility/helpers";
+import data from './assets/data/coffees.json';
 
-devineLog("Hey, ik ben een JS file");
-const arr = [1 , 2,  3];
+{
+    const init = () => {
+        getCoffees(data);
 
-const ES6Stuff = () => devineLog("Ik kan ES6 stuff aan", ...arr);
-ES6Stuff();
+       const $items = document.querySelectorAll(`.price`);
+       $items.forEach(function($item) {
+           $item.addEventListener(`click`, e => {
+                addOrder(e.composedPath()(2));
+           });
+       }); 
+    };
+
+    const getCoffees = data => {
+        const coffees = data.coffees;
+        coffees.forEach(coffee => {
+
+            if (coffees.plantsbased == true) {
+                makeCoffee(coffee);
+            }
+        });
+    };
+
+    const makeCoffee = coffee => {
+        const $lijst = document.querySelector(`.price__list`);
+
+        const $li = document.createElement(`li`);
+        $li.classList.add(`price`);
+        $li.setAttribute(`data_id`, `${coffee.id}`);
+        $li.innerHTML = opbouw(coffee);
+        $lijst.appendChild($li);
+    };
+
+    const opbouw = data => {
+        return `
+        <a class="price__button">
+        <span class="price__button__wrapper">
+          <span class="price__button__name">${data.name}</span>
+          <span class="price__button__amount">&euro; ${round(data.prices.medium)}</span>
+        </span>
+        <span class="price__button__plus">+</span>
+        </a>`;
+    };
+
+
+
+
+};
